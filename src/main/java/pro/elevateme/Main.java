17fb1123c1ea1684bd57984e9e1bb6bc6846af35
Pinky Sodhi <pinky.sodhi@gmail.com>
2026-01-13 13:01:50 -0500

    add functionality to Main class for creating specific Car instances

--- src/main/java/pro/elevateme/Main.java
+++ src/main/java/pro/elevateme/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Car defaultCar = new Car(); // Using the no-argument constructor
         System.out.println("Default Car: " + defaultCar);
+
+        Car specificCar = new Car("Toyota", "Camry", 2020); // Using the parameterized constructor
+        System.out.println("Specific Car: " + specificCar);
     }
 }
